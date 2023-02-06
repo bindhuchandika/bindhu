142b398789259161d98e59707470a512e44fbad0
bindhuchandika <bindhuchandika@gmail.com>
2023-02-06 20:24:13 +0530

    1

---
+++ Jenkins/src/test/java/script/Sample.java
@@ -0,0 +1,25 @@
+package script;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Sample {
+	@Test
+	public void m(){
+		   WebDriverManager.chromedriver().setup();
+		   WebDriver driver = new ChromeDriver();
+		   driver.get("http://localhost:8888/");
+		   driver.findElement(By.name("user_name")).sendKeys("admin");
+		   driver.findElement(By.name("user_password")).sendKeys("admin");
+		   driver.findElement(By.id("submitButton")).click();
+		   driver.findElement(By.xpath("//a[text()='Contacts']")).click();
+		   driver.findElement(By.xpath("//img[@alt='Create Contact...']")).click();
+		   driver.findElement(By.xpath("(//td[@class='small'])[2]")).click();
+	       driver.findElement(By.linkText("Sign Out")).click();
+	       //push
+	}
+}
